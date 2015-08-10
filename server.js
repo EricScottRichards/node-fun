589a1e8c295c36cec3d8e0967ad96e9d6bfa8f2e
EricScottRichards <ericscottrichards@gmail.com>
2015-08-10 13:24:11 -0600

    turrrrrdz

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 ////////////////////////////
 ///     DEPENDENCIES     
 ////////////////////////////
-var express = require('express');
+var express    = require('express');
 var bodyParser = require('body-parser');
+var request    = require('request');
+var firebase   = require('firebase');
+var twilio     = require('twilio');
+var cors       = require('cors');
 
 var app = express();
-var port = 3033;
+var port = process.argv[2] || 8888;
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 
 // app.listen(port, function() {
 //     console.log('I\m watching you...', port);
 // });
 
-app.listen(process.env.PORT, process.env.IP);
-console.log('Powering on...');
+app.listen(port);
+console.log('Powering on...', port);
 console.log('Paired');
 
 
